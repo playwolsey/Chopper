e3b0739db9b59db1ad9539e630e39f6994b04758
cp <chengpengcp9@163.com>
2015-09-01 17:36:36 +0800

    init gulp

---
+++ gulpfile.js
@@ -0,0 +1,76 @@
+/**
+ * @desc gulpfile.js
+ * @author
+ * @date Created on 2015-09-01.
+ */
+
+'use strict';
+
+var gulp = require('gulp');
+var uglify = require('gulp-uglify');
+var concat = require('gulp-concat');
+//var webpack = require('gulp-webpack');
+var webpack = require('webpack');
+var path = require('path');
+
+var server_config = require('./config/webpack.server.config');
+
+var config = {
+    entry: path.resolve(__dirname, 'server/routes.js'),
+    output: {
+        path: path.resolve(__dirname, 'server-dist'),
+        filename: 'test.js'
+    },
+    module: {
+        loaders: [
+            {
+                test: /\.js?$/,
+                exclude: /node_modules/,
+                loader: 'babel'
+            }
+        ]
+    }
+};
+
+gulp.task('build-server', function () {
+    return gulp.src('server')
+        .pipe(webpack(server_config))
+        .pipe(gulp.dest('server-dist'));
+});
+
+gulp.task('build-backend', function(done) {
+    webpack(config).run(function(err, stats) {
+        if (err) {
+            console.log('Error', err);
+        } else {
+            console.log(stats.toString());
+        }
+        done();
+    });
+});
+
+//gulp.task('publish-css', function () {
+//  return gulp.src(['./css/main.css', './css/view.css'])
+//    .pipe(concat('app.css'))
+//    .pipe(shrink())
+//    .pipe(rev())
+//    .pipe(gulp.dest('./build'))
+//    .pipe(qn({
+//      qiniu: qiniu,
+//      prefix: 'gmap'
+//    }))
+//    .pipe(rev.manifest())
+//    .pipe(gulp.dest('./build/rev/css'));
+//});
+
+gulp.task('watch', function () {
+    gulp.watch('./css/*.css', ['css']);
+    gulp.watch('./js/*.js', ['js']);
+});
+
+//gulp.task('publish', function (callback) {
+//  runSequence(
+//    ['publish-css', 'publish-js'],
+//    'publish-html',
+//    callback);
+//});
